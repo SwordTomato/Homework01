169da24e16bdbff5636d92121d50d68da7a7aa14
Sword <phupa001morepg@gmail.com>
2023-05-04 20:42:48 +0700

    Final

--- Program.cs
+++ Program.cs
@@ -5,30 +5,31 @@
              Console.Write("Input number : ");
             int numRows = int.Parse(Console.ReadLine());
 
+            if(numRows >= 0) {
+
             int[][] triangle = new int[numRows][];
 
-//สามเหลี่ยม
             for(int i = 0; i < numRows; i++)
             {
-                triangle[i] = new int[i + 1]; //แปลงจากinput numRows
-                triangle[i][0] = 1; //เซตตัวแรกเป็น 1
+                triangle[i] = new int[i + 1];
+                triangle[i][0] = 1;
 
-                for(int j = 1; j < i; j++) //ดูแกนy
+                for(int j = 1; j < i; j++)
                 {
-                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]; //เอาตัวจากแถวบนบวกกัน
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
                 }
 
-                triangle[i][i] = 1; //เซตตัวท้ายเป็น 1
+                triangle[i][i] = 1;
             }
 
-//ปริ้นค่า
             for(int i = 0; i < numRows; i++)
             {
                 for(int j = 0; j <= i; j++)
                 {
-                    Console.Write(triangle[i][j] + " "); //แกนy
+                    Console.Write(triangle[i][j] + " ");
                 }
                 Console.WriteLine();
             }
-        }
+        } Console.WriteLine("Invalid Pascal’s triangle row number.");
+    }
     }
